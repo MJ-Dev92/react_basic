cec1887d14cf3f40991afc2b890983348d0c8847
MJ-Dev92 <minjae920802@naver.com>
2022-12-14 05:14:02 +0900

    리덕스툴킷md

--- week-3/src/redux/modules/counterSlice.js
+++ week-3/src/redux/modules/counterSlice.js
@@ -15,10 +15,12 @@ const initialState = {
   number: 0,
 };
 
+// slice는 이 createSlice라는 toolkit의 api를 통해서 만들 수가 있습니다.
 const counterSlice = createSlice({
   name: "counter",
   initialState,
   reducers: {
+    // 이 함수들의 이름이 actinValue가 되고
     addNumber: (state, action) => {
       state.number = state.number + action.payload;
     },
@@ -29,5 +31,7 @@ const counterSlice = createSlice({
   },
 });
 
+// 액션크리에이터는 컴포넌트에서 사용하기 위해 export 하고
 export const { addNumber, minusNumber } = counterSlice.actions;
+// reducer는 configStore에 등록하기 위해 export default 합니다.
 export default counterSlice.reducer;
